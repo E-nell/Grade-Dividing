4f5f230d026bc8d664ab787d28864444713fa7d3
E-nell <dlwlxndj@naver.com>
2017-05-26 10:55:39 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+/**
+ * Created by JW on 2017-05-26.
+ */
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+
+        int score;
+        char grade;
+
+        System.out.println("성적을 입력하세요 : ");
+        score = input.nextInt();
+
+
+        if (score >= 90 && score <=100) {
+            grade = 'A';
+        }else if (score >= 80 && score <= 89) {
+            grade = 'B';
+        }else if (score >= 70 && score <= 79) {
+            grade = 'C';
+        }else if (score >= 60 && score <= 69) {
+            grade = 'D';
+        }else if (score < 60) {
+            grade = 'D';
+        }else{
+            grade = '?';
+            System.out.println("성적을 다시 입력하세요요");
+        }
+        System.out.println("성적 : " + score);
+        System.out.println("등급 : " + grade);
+
+    }
+}
